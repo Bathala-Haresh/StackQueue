579e334e57c1a32cb9fe5675536016752c55f663
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-19 01:57:54 +0530

    added:program to add elements to queue

--- Operations.java
+++ Operations.java
@@ -8,9 +8,14 @@ public class Operations {
 	public static int askUser() {
 		System.out.println("Stack Operations ::");
 		System.out.println("Press 1 to Push");
-		System.out.println("Press 2 to display stack");
-		System.out.println("press 3 to exit");
+		System.out.println("Press 2 to Pop");
+		System.out.println("Press 3 to Peek");
+		System.out.println("Press 4 to display stack");
+		System.out.println("Queue Operations ::");
+		System.out.println("Press 5 to Enqueue");
 		
+		System.out.println("Press 7 to display queue");
+		System.out.println("press 8 to quit");
 		int choice = sc.nextInt();
 		return choice;
 	}
@@ -29,12 +34,36 @@ public class Operations {
 				obj.push(push);
 				result = true;
 				break;
+
 			case 2:
-				obj.printStack();
+				obj.pop();
 				result = true;
 				break;
 
 			case 3:
+				System.out.println("Last element is: " + obj.peek());
+				result = true;
+				break;
+
+			case 4:
+				obj.printStack();
+				result = true;
+				break;
+				
+			case 5:
+				System.out.println("Enter element to add for queue : ");
+				int enqueue = sc.nextInt();
+				obj.enqueue(enqueue);
+				result = true;
+				break;
+			
+				
+			case 7:
+				obj.displayQueue();
+				result = true;
+				break;	
+
+			case 8:
 				result = false;
 				break;
 
